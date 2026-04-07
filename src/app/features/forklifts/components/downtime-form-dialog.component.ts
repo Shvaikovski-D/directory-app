707a488b8198cbfc1a15ef6ff6c826be6cba2b71
Shvaikovski-D <dshvaikovski@gmail.com>
2026-04-07 11:23:25 +0300

    Downtime dialog styles

--- src/app/features/forklifts/components/downtime-form-dialog.component.ts
+++ src/app/features/forklifts/components/downtime-form-dialog.component.ts
@@ -32,68 +32,83 @@ export interface DowntimeFormDialogData {
 
     <mat-dialog-content>
       <div class="downtime-form">
-        <!-- НАЧАЛО -->
-        <mat-form-field appearance="outline" class="form-field">
-          <mat-label>начало</mat-label>
+        <!-- ПОЛЯ ДАТЫ И ВРЕМЕНИ -->
+        <div class="date-fields-row">
+          <div style="display: flex; flex-direction: column;">
+            <div class="date-field">
+              <span class="field-label">начало</span>
+              <input
+                matInput
+                type="datetime-local"
+                [value]="startedAt()"
+                (input)="onStartedAtInput($event)"
+              />
+            </div>
 
-          <input
-            matInput
-            type="datetime-local"
-            [value]="startedAt()"
-            (input)="onStartedAtInput($event)"
-          />
+            @if (!isStartedValid()) {
+              <mat-error><small>Поле обязательно</small></mat-error>
+            }
+          </div>
 
-          @if (!isStartedValid() && startedTouched()) {
-            <mat-error>Поле обязательно</mat-error>
-          }
-        </mat-form-field>
-
-        <!-- ОКОНЧАНИЕ -->
-        <mat-form-field appearance="outline" class="form-field">
-          <mat-label>окончание</mat-label>
-
-          <input
-            matInput
-            type="datetime-local"
-            [value]="endedAt() ?? ''"
-            (input)="onEndedAtInput($event)"
-          />
-        </mat-form-field>
+          <div style="display: flex; flex-direction: column;">
+            <div class="date-field">
+              <span class="field-label">окончание</span>
+              <div style="display: flex; flex-direction: column;">
+                <input
+                  type="datetime-local"
+                  [value]="endedAt() ?? ''"
+                  (input)="onEndedAtInput($event)"
+                />
+              </div>
+            </div>
+            @if (!isEndedValid()) {
+              <mat-error><small>Окончание должно быть больше начала</small></mat-error>
+            }
+          </div>
+        </div>
 
         <!-- ОПИСАНИЕ -->
-        <mat-form-field appearance="outline" class="form-field">
-          <mat-label>описание инцидента</mat-label>
+        <div class="date-field-description">
+          <span class="field-label">описание инцидента</span>
           <textarea
-            matInput
-            rows="4"
+            rows="12"
             [value]="description()"
             (input)="onDescriptionInput($event)"
             (blur)="descriptionTouched.set(true)"
           ></textarea>
 
           @if (!isDescriptionValid() && descriptionTouched()) {
-            <mat-error>Минимум 1 символ</mat-error>
+            <mat-error><small>Поле обязательно</small></mat-error>
           }
-        </mat-form-field>
+        </div>
       </div>
     </mat-dialog-content>
 
-    <mat-dialog-actions align="end">
-      <button mat-button color="primary" (click)="onSave()" [disabled]="!isFormValid()">
+    <mat-dialog-actions>
+      <button
+        mat-button
+        class="btn-save"
+        color="primary"
+        (click)="onSave()"
+        [disabled]="!isFormValid()"
+      >
         Сохранить
       </button>
-      <button mat-button (click)="onCancel()">Выход</button>
+      <button mat-button class="btn-cancel" (click)="onCancel()">Выход</button>
     </mat-dialog-actions>
   `,
   styles: `
     :host {
       display: block;
+      background-color: var(--md-sys-color-surface-container-high);
+      padding: 0.5rem;
     }
 
     .dialog-title {
       margin: 0;
       padding: 1rem 1.5rem 0.5rem;
       font-size: 1.25rem;
+      border-bottom: 1px solid red;
     }
 
     .downtime-form {
@@ -104,17 +119,91 @@ export interface DowntimeFormDialogData {
       padding-top: 0.5rem;
     }
 
+    .date-fields-row {
+      display: flex;
+      flex-direction: row;
+      align-items: start;
+      gap: 1rem;
+      margin-bottom: 0.5rem;
+    }
+
+    .date-field {
+      display: flex;
+      align-items: center;
+      gap: 0.5rem;
+      flex: 1;
+    }
+
+    .field-label {
+      padding-right: 0.1rem;
+      white-space: nowrap;
+      font-size: 1.25rem;
+      color: black;
+    }
+
+    .date-field mat-form-field {
+      flex: 1;
+    }
+
     .form-field {
       width: 100%;
+    }
+
+    input {
+      font-size: 1.1rem;
+      padding: 0.2rem 0.5rem;
+      border-radius: 8px;
+      border: 1px solid gray;
+    }
+
+    .date-field-description {
+      display: flex;
+      flex-direction: column;
+
+      > span {
+        padding-bottom: 0.5rem;
+      }
     }
 
     textarea {
       resize: vertical;
       min-height: 80px;
+      border-radius: 8px;
+      font-size: 1.2rem;
     }
 
     mat-dialog-actions {
       padding: 1rem 1.5rem;
+      justify-content: space-around;
+    }
+
+    button[mat-button][disabled] {
+      opacity: 0.5;
+    }
+
+    .btn-cancel {
+      background-color: var(--md-sys-color-on-surface-variant);
+      width: 25%;
+    }
+
+    .btn-save {
+      background-color: var(--md-sys-color-primary) !important;
+      color: var(--md-sys-color-on-primary) !important;
+      width: 25%;
+    }
+
+    .btn-save:hover:not([disabled]),
+    .btn-cancel:hover:not([disabled]) {
+      opacity: 0.9;
+    }
+
+    .btn-save:active:not([disabled]),
+    .btn-cancel:active:not([disabled]) {
+      opacity: 0.8;
+    }
+
+    mat-error {
+      color: red;
     }
   `,
 })
@@ -131,7 +220,16 @@ export class DowntimeFormDialogComponent {
 
   readonly isDescriptionValid = computed(() => this.description().trim().length > 0);
   readonly isStartedValid = computed(() => !!this.startedAt());
-    readonly isFormValid = computed(() => this.isStartedValid() && this.isDescriptionValid());
+  readonly isEndedValid = computed(() => {
+    if (!this.endedAt() || !this.isStartedValid()) return true;
+
+    const start = new Date(this.startedAt()!);
+    const end = new Date(this.endedAt()!);
+    return end > start;
+  });
+  readonly isFormValid = computed(
+    () => this.isStartedValid() && this.isEndedValid() && this.isDescriptionValid(),
+  );
 
   constructor() {
     if (this.data.mode === 'edit' && this.data.downtime) {
